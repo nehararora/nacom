2c0c050deae71eaeb89da1d86d5652082ce1d747
nehararora <nehar.arora@gmail.com>
2016-03-01 23:09:55 -0500

    registered models for admin functionality

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Tag
+from .models import Post
+from .models import Category
+
 # Register your models here.
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Category)
