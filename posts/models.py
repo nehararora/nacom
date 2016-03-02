909de596026f42b077c52a57ed409161ded47b95
nehararora <nehar.arora@gmail.com>
2016-03-01 23:09:28 -0500

    added string representation methods for models

--- posts/models.py
+++ posts/models.py
@@ -11,12 +11,18 @@ class Tag(models.Model):
     tag_name = models.CharField(max_length=255, unique=True)
     tag_description = models.TextField()
 
+    def __str__(self):
+        return self.tag_name
+
 
 class Category(models.Model):
     category_name = models.CharField(max_length=255, unique=True)
     category_tags = models.ManyToManyField(Tag, blank=True, null=True)
     category_description = models.TextField()
     category_content_header = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.category_name
 
 
 class Post(models.Model):
@@ -29,3 +35,6 @@ class Post(models.Model):
     post_comments = models.TextField(blank=True)
     post_mime_type = models.CharField(max_length=256, blank=True)  # TODO: remove??
     post_url = models.SlugField()
+
+    def __str__(self):
+        return self.post_url
