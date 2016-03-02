b2f969764ac7552be0073c1c62f2d15962daf170
nehararora <nehar.arora@gmail.com>
2016-03-01 23:33:03 -0500

    added Post Admin class

--- posts/admin.py
+++ posts/admin.py
@@ -4,7 +4,17 @@ from .models import Tag
 from .models import Post
 from .models import Category
 
-# Register your models here.
+
+# admin models
+class PostAdmin(admin.ModelAdmin):
+    """
+    Post object admin class
+    """
+    # generate slug from heading and content
+    prepopulated_fields = {"post_url": ("post_heading", "post_content")}
+    # save_on_top = True
+
+# Register models.
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
