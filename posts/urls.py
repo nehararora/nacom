0fd53366becc5ee17f201b1fba83b3ec36742edc
nehararora <nehar.arora@gmail.com>
2016-03-06 02:55:45 -0500

    added category urls

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,15 @@ from . import views
 
 urlpatterns = [
     # category views
-    url(r'^$', views.category_view),
+
+    # /home - same as /category/home/1
+    url(r'^$', views.category_view, name='home_view'),
+
+    # /<name> - same as /category/<name>/1
+    url(r'^category/(?P<category_name>\w+)/$', views.category_view, name='home_view'),
+
+    # /category/<name>/<page_number>
+    url(r'^category/(?P<category_name>\w+)/$', views.category_view, name='category_view'),
 
     # tag views
     url(r'^tag/$', views.tag_view),
